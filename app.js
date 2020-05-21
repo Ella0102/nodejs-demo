4c1ba0fe10c6b0e64e45d8d39c9242dbdfdc270f
wssdy <2452036356@qq.com>
2020-05-21 10:03:07 +0800

    change the port

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var store = new SessionStore({
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8000);
 app.set('views', __dirname + '/views');
 app.engine('.html', ejs.__express);
 app.set('view engine', 'html');// app.set('view engine', 'ejs');
